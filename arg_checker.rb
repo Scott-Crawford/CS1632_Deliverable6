62c775cb3b2d50531bafb3274c0e0a2ec7f5b005
Sumanyu Gupta <sug27@pitt.edu>
2018-04-12 19:52:24 -0400

    More functionality added. Rubocop errors hella bad.

--- arg_checker.rb
+++ arg_checker.rb
@@ -43,15 +43,18 @@ class ArgsChecker
   def define_variable(input)
     @map = {} if @map.nil?
     items = input[2..input.length - 1]
-    @map[input[1]] = do_math(items)
+    val = do_math(items)
+    @map[input[1].upcase] = val[0] unless val.empty?
+    puts @map[input[1].upcase] unless val.empty?
+    @stack.clear
   end
 
   def check_first_element(input)
-    input[0] = input[0].upcase
-    if %w[LET PRINT QUIT].include?(input[0])
-      if input[0] == 'QUIT'
+    first_element = input[0].upcase
+    if %w[LET PRINT QUIT].include?(first_element)
+      if first_element == 'QUIT'
         exit!
-      elsif input[0] == 'LET'
+      elsif first_element == 'LET'
         define_variable(input)
       else
         do_math(input[1..input.length - 1])
@@ -60,30 +63,43 @@ class ArgsChecker
     end
   end
 
-  def check_variable_in_map(input)
-    input.each do |var|
-      puts "#{var} is not initialized" unless @map.key?(var)
+  def call_error(code, var)
+    puts "Line BLAH: #{var} is not initialized" if code == 1
+    @stack.clear
+  end
+
+  def handle_more(input, val)
+    if input.length == 1 && input[0].length == 1 && input[0].match(/[a-zA-Z]/) && @map.key?(input[0].upcase)
+      puts @map[input[0].upcase]
+    else
+      do_math(input) unless val
+      puts @stack[0] unless @stack.empty? || @stack[0].nil?
     end
   end
 
   def handle_input(input)
     @stack = []
     input = input.split(' ')
-    puts @map[input[0]] if input.length == 1 && input[0].length == 1 && input[0].match(/[a-zA-Z]/)
-    do_math(input) unless check_first_element(input)
-    puts @stack
+    val = check_first_element(input)
+    handle_more(input, val)
   end
 
   def do_math(input)
     input.each do |i|
       if %w[+ - * /].include?(i)
         handle_operators(i)
+      elsif i.length == 1 && i.match(/[a-zA-Z]/)
+        if @map.key?(i.upcase)
+          @stack.push(@map[i.upcase])
+        else
+          call_error(1, i)
+          return []
+        end
       else
-        # puts "Line #{@line_counter}: Unkown keyword #{i}"
         @stack.push(i)
       end
     end
-    @stack[0]
+    @stack
   end
 
   def handle_operators(opt)
@@ -100,6 +116,7 @@ class ArgsChecker
   end
 
   def run_repl
+    @map = {} if @map.nil?
     @stack = [] if @stack.nil?
     repl = lambda { |prompt|
       print prompt
